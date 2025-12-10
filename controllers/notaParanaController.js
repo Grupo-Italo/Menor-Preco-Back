6bc93b4e1fa42de32bbe015dea7d80a650de4083
Patrick Souza <patrick.souza@superdia.com.br>
2025-12-10 10:02:54 -0300

    Filtro por raio adicionado

--- controllers/notaParanaController.js
+++ controllers/notaParanaController.js
@@ -3,8 +3,21 @@ const productsModel = require('../models/productsModel');
 
 exports.search = async (req, res) => {
     try {
-        const { local, gtin, termo } = req.query;
+        const { local, gtin, termo, raio } = req.query;
         const data = await searchService.search(local, gtin, termo);
+
+        if (raio && data?.produtos?.length) {
+            const raioMeters = Number(raio);
+            if (!Number.isNaN(raioMeters)) {
+                data.produtos = data.produtos.filter(prod => {
+                    let distVal = prod.distkm;
+                    if (typeof distVal === 'string') distVal = distVal.replace(',', '.');
+                    const distKm = parseFloat(distVal);
+                    if (Number.isNaN(distKm)) return false;
+                    return (distKm * 1000) <= raioMeters;
+                });
+            }
+        }
 
         if (gtin && data?.produtos?.length) {
             const produtosBulk = data.produtos.map(produto => ({
@@ -25,9 +38,9 @@ exports.search = async (req, res) => {
 
             await productsModel.createOrUpdateProductsBulk(produtosBulk);
         }
+
         res.json(data);
     } catch (err) {
-        
         res.status(500).json({ error: err.message });
     }
 };
